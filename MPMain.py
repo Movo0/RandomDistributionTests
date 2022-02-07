fa876657ce5646d73ae902933067e641165e39d4
Movo0 <95084005+Movo0@users.noreply.github.com>
2022-02-07 23:22:09 +0100

    Update MPMain.py
    A little explanation ,shortening and fixing

--- MPMain.py
+++ MPMain.py
@@ -11,55 +11,49 @@ methods=['Random','Numpy','BadRandom','LCG']
 #if __name__ == "__main__":
     #sampals=10**int(input("Number of Samples in 10**x:"))
     #runrange=int(input("Run Range:"))
-sampals=10**6
+sampals=10**7
 runrange=1000
 
 l= int(len(methods))
 DIS=[[]for i in range(l)]
 CHI=[0 for i in range(l)]
 
-def LCG(min,max,seed):
+def LCG(min,max,seed):#A purposefully bad random number generator with an pretty good distribution but bad in a bitmap
     a = 7
     c = 31
     LCG = ((a*seed)+c) % runrange
     return int(LCG)
 
-def bad_random(min,max,seed):
+def bad_random(min,max,seed):#A purposefully bad random number generator with an pretty bad distribution but good in a bitmap
     c=round(max/2+((max+min)/2 * (math.cos(math.pi*math.cos(5000*seed**2)))))
     return int(c)
 
-def CreateDistribution(type):
+def CreateDistribution(type):#creates the distribution and puts it in an array. Ex. x is 4 and DIS[4] gets one added to it
     DIS = [0 for i in range(runrange)]
     if type==0:
         start=time.time()
         for i in range(sampals):
             x = rn.randint(0, len(DIS)-1)
             DIS[x] = DIS[x]+1
-        print(methods[type]+" took: "+str(round(time.time()-start))+" s")
-        return DIS
     if type==1:
         start=time.time()
         for i in range(sampals):
             x = np.random.randint(0, len(DIS))
             DIS[x] = DIS[x]+1
-        print(methods[type]+" took: "+str(round(time.time()-start))+" s")
-        return DIS
     if type==2:
         start=time.time()
         for i in range(sampals):
             x = bad_random(0, len(DIS)-1,i)
             DIS[x] = DIS[x]+1
-        print(methods[type]+" took: "+str(round(time.time()-start))+" s")
-        return DIS
     if type==3:
         start=time.time()
         for i in range(sampals):
             x = LCG(0, len(DIS)-1,i)
             DIS[x] = DIS[x]+1
-        print(methods[type]+" took: "+str(round(time.time()-start))+" s")
-        return DIS
+    print(methods[type]+" took: "+str(round(time.time()-start))+" s")
+    return DIS
 
-def ChiCalc(DIS):
+def ChiCalc(DIS):#does the chi calculation
     chi=0
     for i in range(runrange):
         x=(DIS[i]-(sampals/runrange))
@@ -67,12 +61,15 @@ def ChiCalc(DIS):
     chi= chi/runrange
     return float(chi)
 
-def Output(DIS,CHI):
+def Output(DIS,CHI):#does the terminal output
     print()
-    for i in range(0,len(methods)):
-        print(methods[i]+" Distribution")
-        print(DIS[i])
-        print()
+    Answer=input("Print Distribution? : ")
+    if Answer=="Y"or Answer=="y"or Answer=="yes"or Answer=="Yes":
+        for i in range(0,len(methods)):
+            print(methods[i]+" Distribution")
+            print(DIS[i])
+            print()
+    print()
     for i in range(0,len(methods)):
         print(methods[i]+" Chi")
         print(CHI[i])
@@ -80,7 +77,7 @@ def Output(DIS,CHI):
     print("Number of sampals: "+str(sampals))
     print("Range: "+str(runrange))
 
-def Plot(DIS,CHI):
+def Plot(DIS,CHI):#does the ploting
     plt.figure(figsize=(9, 2))
 
     plt.subplot(121)
@@ -89,20 +86,17 @@ def Plot(DIS,CHI):
 
     plt.subplot(122)
     plt.ylim([0.8*sampals/runrange, 1.2*sampals/runrange])
-    for i in range(len(methods)):
-        x=[j for j in range(runrange)]
-        plt.plot(x, DIS[i], label=methods[i])
+    X=[j for j in range(runrange)]
+    for i in range(len(methods)):plt.plot(X, DIS[i], label=methods[i])
+    plt.savefig("test.png")
     plt.show()
 
 if __name__ == '__main__':
-    # start 4 worker processes
-    with mp.Pool(processes=l) as pool:
-        multiple_results = [pool.apply_async(CreateDistribution, (i,)) for i in range(4)]
-        time.sleep(0.1)
-        pool.join
-        DIS=[res.get() for res in multiple_results]
-        pool.close
-    for i in range(len(methods)):
-        CHI[i]=[ChiCalc(DIS[i])]
+    with mp.Pool(processes=l) as pool:#starts a a process for every method
+        multiple_results = [pool.apply_async(CreateDistribution, (i,)) for i in range(4)]#starts async calculations
+        DIS=[res.get() for res in multiple_results]#gets data
+        pool.close#closes the pool
+    for i in range(len(methods)):CHI[i]=ChiCalc(DIS[i])#does the chi calculation
+
     Output(DIS,CHI)
     Plot(DIS,CHI)
